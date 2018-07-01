24e93048a7519d84bea4368f63c958c7b6f6f405
Diaz Agasatya <diazagasatya@gmail.com>
2018-07-01 00:39:12 -0700

    Compression & Decompression

---
+++ src/compression_and_decompression/Decompression.java
@@ -0,0 +1,143 @@
+package compression_and_decompression;
+
+/**
+ * This class will contain methods that will help client
+ * to decompress string compression in a form of frequency and
+ * string representation letters.
+ * i.e 2[3[a]b]
+ * @author Diaz Agasatya        June 29th 2018
+ */
+public class Decompression {
+
+   // Static strings that will be used throughout the recursion
+   private static String decompressedString = "";
+   private static String coreString = "";
+
+   /*
+       * Constraints given:
+       * - Number can have more than one digit. For example, 10[a] is allowed, and just means aaaaaaaaaa
+       * - One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
+       * - Characters allowed as input include digits, small English letters and brackets [ ].
+       * - Digits are only to represent amount of repetitions.
+       * - Brackets are only part of syntax of writing repeated substring.
+       * - Input is always valid, so no need to check its validity.
+       * Hints (Recursion, Loops, Strings, Algorithm) will be useful for this case
+       */
+
+   /**
+    * Public method decompress where it will check validation, then call the protected
+    * recursion method, and return the complete decompressed String.
+    * @param compressedString          Compressed String
+    * @return decompressedString
+    */
+   public String decompress(String compressedString) {
+
+      // Make sure that the string is not null
+      if(compressedString.length() == 0 || compressedString.equals(null)) {
+         return "Please insert valid compressed string (i.e 3[ab]c, 3[6[4[aew]d]r])";
+      }
+
+      // Call the protected recursive method to decompress the string
+      decompress(compressedString, 0, "", 0, compressedString.length() - 1);
+
+      if(decompressedString.length() != 0) {
+         return decompressedString;
+      }
+
+      // If decompression failed
+      return "Decompression failed, please try again using valid compression patterns";
+   }
+
+   /**
+    * Protected decompression called, this is a recursive call that will follow
+    * the "Peel The Onion" implementation or Dividing and Conquer the inner, outer
+    * and core string of compression.
+    * @param cString             Compressed String
+    * @param repetition          Repetition for every onion peel (recursion)
+    * @param outerString         Outer String needed to be added to combination
+    * @param start               Starting Index of each peel / recursion
+    * @param end                 Ending Index of each peel / recursion
+    */
+   protected void decompress(String cString, int repetition ,
+                             String outerString, int start, int end) {
+
+      // Base Condition
+      if(Character.isLetter(cString.charAt(start))) {
+         return;
+      }
+
+      // Initialize variables & counters
+      boolean digitValidation = false;
+      int i = start;
+      // ========= Get the starting digit for initializing repetition========
+      while (!digitValidation && i < end) {
+         if (cString.charAt(i) == '[') {
+            repetition = Integer.parseInt(cString.substring(start, i));
+            // System.out.println("REPEAT = " + repetition); // debugging
+            start = i + 1;
+            digitValidation = true;
+         }
+         i++;
+      }
+
+      // =====================Get the outerString============================
+      boolean oStringValidation = false;
+      int j = end;
+      if(cString.charAt(end) == ']') {
+         outerString = "";
+         end--;
+      } else {
+         while(!oStringValidation) {
+            if(cString.charAt(j) == ']') {
+               outerString = cString.substring(j + 1,end + 1);
+               // System.out.println("OUTERSTRING = " + outerString); (debugging)
+               end = j - 1;
+               oStringValidation = true;
+            }
+            j--;
+         }
+      }
+
+      // If recursed until core compression, grab the inner and
+      int k = start - 1;
+      int sCoreStringIndex = 0;
+      int compressionSkin = 0;
+      while (compressionSkin < 2) {
+         if(cString.charAt(k) == '[') {
+            sCoreStringIndex = k + 1;
+            compressionSkin++;
+         }
+         if(cString.charAt(k) == ']') {
+            coreString = cString.substring(sCoreStringIndex, k);
+            break;
+         }
+         k++;
+      }
+
+      // Recursion starts here
+      decompress(cString, repetition, outerString, start, end);
+
+      // The real combination decompression starts here
+      decompressString(repetition, outerString, coreString);
+   }
+
+   /**
+    * Helper method of decompress recursion to merge the combinations from each
+    * peel into one big decompressed string.
+    * @param repetition          Repetition of combination each peel
+    * @param outerString         OuterString needed to be concatenated
+    * @param innerString         InnerString needed to be repeated by n repetition
+    */
+   private void decompressString(int repetition, String outerString, String innerString) {
+      String combination = "";
+      for(int i = 0; i < repetition; i++) {
+         combination += innerString;
+      }
+      combination += outerString;
+      // System.out.println("Combination = " + combination); // debugging
+      coreString = combination;
+      decompressedString = combination;
+   }
+
+
+}
---
+++ src/compression_and_decompression/TestDecompression.java
@@ -0,0 +1,126 @@
+package compression_and_decompression;
+
+/**
+ * Coding Challenge based upon Google's Tech Dev Guide
+ * https://techdevguide.withgoogle.com/paths/advanced/compress-decompression#code-challenge
+ * This class will test the Decompression class & Includes 5 test cases.
+ * @author Diaz Agasatya        June 29th 2018
+ */
+public class TestDecompression {
+
+   /**
+    * Main Test for Class Decompression is tested here
+    * @param args
+    */
+   public static void main(String[] args) {
+
+      /*
+       * Constraints given:
+       * - Number can have more than one digit. For example, 10[a] is allowed, and just means aaaaaaaaaa
+       * - One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
+       * - Characters allowed as input include digits, small English letters and brackets [ ].
+       * - Digits are only to represent amount of repetitions.
+       * - Brackets are only part of syntax of writing repeated substring.
+       * - Input is always valid, so no need to check its validity.
+       * Hints (Recursion, Loops, Strings, Algorithm) will be useful for this case
+       */
+      // Create an instance of class Decompression
+      Decompression decompression = new Decompression();
+
+      // Success tests
+      int numberOfSuccess = 0;
+
+      // Test 1
+      String testOne = "3[ab]";
+      String expectedResult = "ababab";
+      String testOneSolution = decompression.decompress(testOne);
+
+      // Test 1 Validation
+      if(testOneSolution.equals(expectedResult)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Test 1 Unsuccessful");
+      }
+
+      // Test 2
+      String testTwo = "2[3[ats]b]";
+      String expectedResultTwo = "atsatsatsbatsatsatsb";
+      String testTwoSolution = decompression.decompress(testTwo);
+
+      // Test 2 Validation
+      if(testTwoSolution.equals(expectedResultTwo)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Test 2 Unsuccessful");
+      }
+
+      // Test 3
+      String testThree = "10[uvk]";
+      String expectedResultThree = "uvkuvkuvkuvkuvkuvkuvkuvkuvkuvk";
+      String testThreeSolution = decompression.decompress(testThree);
+
+      // Test 3 Validation
+      if(testThreeSolution.equals(expectedResultThree)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Test 3 Unsuccessful");
+      }
+
+      // Test 4
+      String testFour = "2[7[3[ab]c]d]";
+      String expectedResultFour = "abababcabababcabababcabababcabababcabababcabababcd" +
+            "abababcabababcabababcabababcabababcabababcabababcd";
+      String testFourSolution = decompression.decompress(testFour);
+
+      if(testFourSolution.equals(expectedResultFour)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Test 4 Unsuccessful");
+      }
+
+      // Test 5
+      String testFive = "1[6[3[2[5[io]d]asd]qwy]rwq]";
+      String expectedResultFive = "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwy" +
+            "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwy" +
+            "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwy" +
+            "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwy" +
+            "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwy" +
+            "ioioioioiodioioioioiodasdioioioioiodioioioioiodasdioioioioiodioioioioiodasdqwyrwq";
+      String testFiveSolution = decompression.decompress(testFive);
+
+      if(testFiveSolution.equals(expectedResultFive)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Test 5 Unsuccessful");
+      }
+
+      // =============================EDGE TEST CASES HERE===================================
+      String edgeCaseOne = "a[]b";
+      String expectedResultCaseOne = "Please insert valid compressed string (i.e 3[ab]c, 3[6[4[aew]d]r])";
+      String edgeCaseTestOne = decompression.decompress(edgeCaseOne);
+
+      if(edgeCaseTestOne.equals(expectedResultCaseOne)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Edge Case One Failed!");
+      }
+
+      String edgeCaseTwo = "0[abc]";
+      String expectedResultCaseTwo = "Please insert valid compressed string (i.e 3[ab]c, 3[6[4[aew]d]r])";
+      String edgeCaseTestTwo = decompression.decompress(edgeCaseTwo);
+
+      if(edgeCaseTestTwo.equals(expectedResultCaseTwo)) {
+         numberOfSuccess++;
+      } else {
+         System.out.println("Edge Case Two Failed!");
+      }
+
+      // ===============================Success Run(s)===================================
+      if(numberOfSuccess == 7) {
+         System.out.println("Compression & Decompression Test is Successful!");
+      } else {
+         System.out.println("Compression & Decompression Test is NOT successful" +
+               ", please check your algorithm");
+      }
+   }
+}
